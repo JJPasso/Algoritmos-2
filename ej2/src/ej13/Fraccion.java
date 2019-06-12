6c5d93f0cc96a4d7f348f090cf144dd7d3eb86c6
AluDoc <AluDoc@LABO>
2019-06-12 13:05:24 -0300

    hasta el 13 incompleto

--- ej2/src/ej13/Fraccion.java
+++ ej2/src/ej13/Fraccion.java
@@ -18,5 +18,52 @@ public class Fraccion {
 	public void MostrarFraccion() {
 		System.out.println(numerador+"/"+denominador);
 	}
-	
+	private double getDenominador(){
+		return denominador;
+	}
+	private double getNumerador() {
+		return numerador;
+	}
+	public Fraccion suma(Fraccion n1,Fraccion n2) {
+		double n;
+		double d;
+		if (n1.getDenominador()==n1.getDenominador()) {
+			n = n1.getNumerador()+n2.getNumerador();
+			d = n1.getDenominador();
+		}else {
+			d = n1.getDenominador()*n2.getDenominador();
+			double q1 = n2.getDenominador()*n1.getNumerador();
+			double q2 = n2.getNumerador()*n1.getDenominador();
+			n = q1 + q2;
+			}
+		Fraccion f = new Fraccion(n,d);
+		return f;
+	}
+	public Fraccion resta(Fraccion n1,Fraccion n2) {
+		double n;
+		double d;
+		if (n1.getDenominador()==n1.getDenominador()) {
+			n = n1.getNumerador()-n2.getNumerador();
+			d = n1.getDenominador();
+		}else {
+			d = n1.getDenominador()*n2.getDenominador();
+			double q1 = n2.getDenominador()*n1.getNumerador();
+			double q2 = n2.getNumerador()*n1.getDenominador();
+			n = q1 - q2;
+			}
+		Fraccion f = new Fraccion(n,d);
+		return f;
+	}
+	public Fraccion Multiplicacion(Fraccion n1,Fraccion n2) {
+		double n = n1.getNumerador()*n2.getNumerador();
+		double d = n1.getDenominador()*n2.getDenominador();
+		Fraccion f = new Fraccion(n,d);
+		return f;
+	}
+	public Fraccion Division(Fraccion n1, Fraccion n2) {
+		double n = n1.getDenominador()*n2.getNumerador();
+		double d = n1.getNumerador()*n2.getDenominador();
+		Fraccion f = new Fraccion(n,d);
+		return f;
+	}
 }
