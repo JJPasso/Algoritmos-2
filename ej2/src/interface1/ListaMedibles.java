421681ce4c1f55b9f80107c62abbe43b9241443a
Master Race <Master Race@DESKTOP-CUJECKT>
2019-06-23 00:31:26 -0300

    qsyo bb

---
+++ ej2/src/interface1/ListaMedibles.java
@@ -0,0 +1,7 @@
+package interface1;
+
+import java.util.ArrayList;
+
+public class ListaMedibles {
+	ArrayList<MedidorDePresion,Termometro> Medi;
+}
---
+++ ej2/src/interface1/Medibles.java
@@ -0,0 +1,5 @@
+package interface1;
+
+public enum Medibles {
+	CuentaBancaria,MedidorDePresion,Termometro
+}
